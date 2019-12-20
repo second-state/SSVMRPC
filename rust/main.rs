bcecd7c6d786e94aad8c440ce87c7126435d143e
tpmccallum <mistermac2008@gmail.com>
2019-12-20 13:05:24 +1000

    auto update

--- rust/main.rs
+++ rust/main.rs
@@ -236,8 +236,8 @@ fn execute_wasm_function(bytes_vec: Data) -> content::Json<String>{
                 &fs,
                 application_uuid.unwrap(),
                 function_name.unwrap(),
-                &function_arguments, // As a vector of strings
-                &modules, // As a vector of strings
+                function_arguments, // As a vector of strings
+                modules, // As a vector of strings
             );
             content::Json(response)
             
