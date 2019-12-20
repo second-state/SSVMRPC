aa1c809051d24e8e882822d379f97a1c1853a4ee
tpmccallum <mistermac2008@gmail.com>
2019-12-20 12:24:43 +1000

    auto update

--- rust/main.rs
+++ rust/main.rs
@@ -216,12 +216,12 @@ fn execute_wasm_function(bytes_vec: Data) -> content::Json<String>{
         let function_arguments = &v["request"]["function"]["arguments"];
         println!("Function arguments: {:?}", function_arguments);
         // Turn the list of arguments into a Vector so that we can pass this as part of the agrument
-        let args = function_arguments.iter().map(|&s| s.into()).collect();
+        let args = function_arguments.unwrap.iter().map(|&s| s.into()).collect();
         // Wasm modules
         let modules = &v["request"]["modules"];
         println!("Wasm modules: {:?}", modules);
         // Turn the list of modules into a Vector so that we can pass this as part of the agrument
-        let mods = modules.iter().map(|&s| s.into()).collect();
+        let mods = modules.unwrap.iter().map(|&s| s.into()).collect();
 
         // Evaluate the storage options
         //if application_storage.to_owned() == Some("file_system") {
