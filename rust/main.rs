642f09de36ba45a292553cecff78540a6e9d9843
tpmccallum <mistermac2008@gmail.com>
2020-01-03 17:54:34 +1000

    auto update

--- rust/main.rs
+++ rust/main.rs
@@ -277,7 +277,7 @@ fn execute_wasm_function(bytes_vec: Data) -> content::Json<String>{
 
             println!("Application storage is being set to the default: file_system.");
             
-            let response = ssvm_container::storage::file_system::FileSystem::execute_ewasm_function(
+            let response = ssvm_container::storage::file_system::FileSystem::execute_wasm_function(
                 &fs,
                 application_uuid.unwrap(),
                 modules,
